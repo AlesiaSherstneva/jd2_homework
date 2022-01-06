79d1d49bd3e5aab21495ff6afa0b218249350464
AlesiaSherstneva <91740141+AlesiaSherstneva@users.noreply.github.com>
2022-01-06 10:35:25 +0300

    Add files via upload

--- Module_2_task_9/src/main/java/web/VisitsCounterServlet.java
+++ Module_2_task_9/src/main/java/web/VisitsCounterServlet.java
@@ -6,27 +6,59 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class VisitsCounterServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private int visitNumber;
+    private boolean fileExists;
+    String path = "C:" + File.separator + "Temp"+ File.separator;
 
     public void init() {
         visitNumber = Integer.parseInt(getServletConfig().getInitParameter("visitNumber"));
+        File file = new File(path + "count.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            visitNumber += scanner.nextInt();
+            fileExists = true;
+        } catch (FileNotFoundException fileNotFoundException) {
+            try {
+                boolean mkdir = new File(path).mkdir();
+                fileExists = file.createNewFile();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
     }
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        visitNumber++;
         PrintWriter out = response.getWriter();
         out.println("<html><head><title>Visits Counter Servlet</title><head>");
         out.println("<body><h1>Welcome to my page!</h1>");
-        out.println("<p> Your visit number is ");
-        out.println(visitNumber);
-        out.println("</p></body></html>");
+        if(fileExists) {
+            visitNumber++;
+            out.println("<p> Your visit number is ");
+            out.println(visitNumber);
+            out.println("</p></body></html>");
+        } else {
+            out.println("<p> Sorry, I can't count your visits :-( </p>");
+            out.println("<p> Try to visit my page from another device </p>");
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse responce) throws ServletException, IOException {
         doGet(request, responce);
+    }
+
+    public void destroy() {
+        File file = new File(path + "count.txt");
+        try {
+            PrintWriter writer = new PrintWriter(file);
+            writer.println(visitNumber);
+            writer.close();
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
     }
 }
