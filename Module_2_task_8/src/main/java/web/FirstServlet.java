de685b3064282cedd1a8a843982c8005797fa8ef
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-04-20 19:33:05 +0300

    Edit task8

--- Module_2_task_8/src/main/java/web/FirstServlet.java
+++ Module_2_task_8/src/main/java/web/FirstServlet.java
@@ -1,12 +1,13 @@
 package web;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@SuppressWarnings("RedundantThrows")
 public class FirstServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
