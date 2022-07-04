99e456c52295a6c52d6ca627de85f21eecc8d7fd
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-04 18:16:48 +0300

    Update loader

--- Module_3_task_2-3/src/main/java/by/academy/it/loader/PersonLoader.java
+++ Module_3_task_2-3/src/main/java/by/academy/it/loader/PersonLoader.java
@@ -36,11 +36,28 @@ public class PersonLoader {
 
         savePerson(session, person);
 
-        getPerson(session, person.getId());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-        loadPerson(session, person.getId());
+        person = getPerson(session, person.getId());
+        System.out.println("The person who was got by get() method:\n" + person.getName() + " "
+                + person.getSurname() + " " + person.getAge() + " years old");
+        System.out.println("Person id = " + person.getId());
+        System.out.println("Trigger: person was created at " + person.getTimestamp());
 
-        deletePerson(session, person.getId());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        person = loadPerson(session, person.getId());
+        System.out.println("The person who was got by load() method:\n" + person.getName() + " "
+                + person.getSurname() + " " + person.getAge() + " years old");
+        System.out.println("Person id = " + person.getId());
+        System.out.println("Trigger: person was created at " + person.getTimestamp());
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        long id = deletePerson(session, person.getId());
+        System.out.println("The person with id = " + id + " was deleted successfully");
+        System.out.println("The person who was got by get() method: " + getPerson(session, id));
+        System.out.println("The person who was got by load() method: " + loadPerson(session, id));
 
         session.getTransaction().commit();
         session.close();
@@ -59,7 +76,6 @@ public class PersonLoader {
         //System.out.println(session.isDirty()); //return false
 
         session.refresh(person);
-
         // Если запустить refresh() без предварительного flush(), то триггерное время сотрётся
 
         return id;
@@ -80,11 +96,12 @@ public class PersonLoader {
         return loadedPerson;
     }
 
-    public static void deletePerson(Session session, Long id) {
+    public static long deletePerson(Session session, Long id) {
         try {
             session.delete(session.get(Person.class, id));
         } catch (ObjectNotFoundException | IllegalArgumentException exception) {
             exception.printStackTrace();
         }
+        return id;
     }
 }
