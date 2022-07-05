29eeb8ea20172b58e0cdae02219630a536562434
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-05 14:38:40 +0300

    Edit pojo

--- Module_4_task_8-9-10/src/main/java/by/academy/it/beansForExcluding/AnotherPerson.java
+++ Module_4_task_8-9-10/src/main/java/by/academy/it/beansForExcluding/AnotherPerson.java
@@ -3,7 +3,6 @@ package by.academy.it.beansForExcluding;
 import by.academy.it.interfaces.IAddress;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
